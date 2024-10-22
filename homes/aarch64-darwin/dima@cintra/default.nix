28bf5d328d8cdbb004c4ae1485c6f027f37fcf02
Dmitrii Miaskovskii <dimonzozo@gmail.com>
2024-10-22 02:36:00 +0200

    Add new host

---
+++ homes/aarch64-darwin/dima@cintra/default.nix
@@ -0,0 +1,43 @@
+{
+  ...
+}:
+{
+  internal = {
+    user = {
+      enable = true;
+      name = "dima";
+      fullName = "Dmitrii Miaskovskii";
+      email = "dmitry@enapter.com";
+    };
+
+    cli-apps = {
+      fish = {
+        enable = true;
+
+        # TODO: add assert
+        # requires tools.sops.enable == true;
+        manageSshKeys = true;
+        manageSshConfig = true;
+        atuinSync = true;
+        includeSecretAliases = true;
+      };
+      tmux.enable = true;
+      neovim.enable = true;
+      home-manager.enable = true;
+    };
+
+    desktop.macos.enable = true;
+
+    tools = {
+      git.enable = true;
+      common.enable = true;
+      alacritty.enable = true;
+      sops.enable = true;
+    };
+  };
+
+  home.sessionPath = [ "$HOME/bin" ];
+
+  home.stateVersion = "22.05";
+}
+
---
+++ systems/aarch64-darwin/cintra/default.nix
@@ -0,0 +1,20 @@
+{
+  ...
+}:
+{
+  internal = {
+    suites = {
+      common.enable = true;
+      development.enable = true;
+    };
+
+    user = {
+      name = "dima";
+      fullName = "Dmitrii Miaskovskii";
+      email = "dmitry@enapter.com";
+      uid = -1;
+    };
+  };
+
+  system.stateVersion = 4;
+}
